8e0328f8f2555522508b2dde5a53077722bef436
Tonniky <224966919@qq.com>
2020-05-21 16:53:53 +0800

    Initial commit

---
+++ src/main/java/actredis/acfu/AcfuApplication.java
@@ -0,0 +1,13 @@
+package actredis.acfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcfuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcfuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/actredis/acfu/AcfuApplicationTests.java
@@ -0,0 +1,13 @@
+package actredis.acfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcfuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
